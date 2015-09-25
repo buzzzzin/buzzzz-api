ccfa9e2224433379a1cb49248267358174a8423e
sanjeev <sanjeev.jha@intelligrape.com>
2015-09-26 02:46:12 +0530

    code refactored

--- src/main/java/in/buzzzz/repository/tag/TagRepository.java
+++ src/main/java/in/buzzzz/repository/tag/TagRepository.java
@@ -1,9 +1,8 @@
 package in.buzzzz.repository.tag;
 
-import in.buzzzz.domain.mapping.UserAuthMapping;
 import in.buzzzz.domain.tag.Tag;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface TagRepository extends MongoRepository<Tag, String> {
-    UserAuthMapping findByTag(String email);
+    Tag findByTag(String tag);
 }
