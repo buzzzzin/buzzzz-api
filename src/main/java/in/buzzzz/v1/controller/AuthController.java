ba72fe7f166f269e16585dae2a631f6ccf8f6358
Jeevesh Pandey <jeevesh.pandey@tothenew.com>
2015-09-25 21:59:11 +0530

    Modification:Added internationalization.

--- src/main/java/in/buzzzz/v1/controller/AuthController.java
+++ src/main/java/in/buzzzz/v1/controller/AuthController.java
@@ -9,31 +9,32 @@ import in.buzzzz.v1.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-/**
- * Created by ekansh on 24/9/15.
- */
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
-    @Autowired private PrepareAuthResponseService prepareAuthResponseService;
-    @Autowired private PrepareErrorResponseService prepareErrorResponseService;
-    @Autowired private UserService userService;
+    @Autowired
+    private PrepareAuthResponseService prepareAuthResponseService;
+    @Autowired
+    private PrepareErrorResponseService prepareErrorResponseService;
+    @Autowired
+    private UserService userService;
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public ResponseDto login(@RequestBody User user){
+    public ResponseDto login(@RequestBody User user,
+                             @RequestHeader(value = "Accept-Language", defaultValue = "UK") String locale) {
         System.out.println(user.toString());
-        return prepareAuthResponseService.loginResponse(userService.save(user));
+        return prepareAuthResponseService.loginResponse(userService.save(user), locale);
     }
 
     @RequestMapping(value = "/logout", method = RequestMethod.POST)
-    public ResponseDto logout(){
-
-        return prepareAuthResponseService.logoutResponse(null);
+    public ResponseDto logout(
+            @RequestHeader(value = "Accept-Language", defaultValue = "UK") String locale) {
+        return prepareAuthResponseService.logoutResponse(null, locale);
     }
 
     @ExceptionHandler(GenericException.class)
-    private ResponseDto catchException(GenericException e){
+    private ResponseDto catchException(GenericException e) {
         return prepareErrorResponseService.catchException(e);
     }
 
--- src/main/java/in/buzzzz/v1/response/PrepareAuthResponseService.java
+++ src/main/java/in/buzzzz/v1/response/PrepareAuthResponseService.java
@@ -2,23 +2,31 @@ package in.buzzzz.v1.response;
 
 import in.buzzzz.util.messages.SuccessCodes;
 import in.buzzzz.v1.data.response.ResponseDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
-/**
- * Created by ekansh on 25/9/15.
- */
+import java.util.Locale;
+
 @Service
 public class PrepareAuthResponseService {
 
-    public ResponseDto loginResponse(Object data){
+    @Autowired
+    private MessageSource messageSource;
+
+    private Locale getLocale(String locale) {
+        return new Locale(locale);
+    }
+
+    public ResponseDto loginResponse(Object data, String locale) {
         ResponseDto responseDto = new ResponseDto(data);
-        responseDto.setMessage(SuccessCodes.LOGIN_SUCCESS);
+        responseDto.setMessage(messageSource.getMessage(SuccessCodes.LOGIN_SUCCESS, null, getLocale(locale)));
         return responseDto;
     }
 
-    public ResponseDto logoutResponse(Object data){
+    public ResponseDto logoutResponse(Object data, String locale) {
         ResponseDto responseDto = new ResponseDto(data);
-        responseDto.setMessage(SuccessCodes.LOGOUT_SUCCESS);
+        responseDto.setMessage(messageSource.getMessage(SuccessCodes.LOGOUT_SUCCESS, null, getLocale(locale)));
         return responseDto;
     }
 
