1755337a5308e3cfd73f36ceb7789151bcc62d70
sanjeev <sanjeev.jha@intelligrape.com>
2015-09-25 23:09:52 +0530

    created tag domain class

--- src/main/java/in/buzzzz/domain/tag/Tag.java
+++ src/main/java/in/buzzzz/domain/tag/Tag.java
@@ -1,11 +1,60 @@
 package in.buzzzz.domain.tag;
 
-/**
- * Created by ekansh on 25/9/15.
- */
+import java.util.Date;
+
 public class Tag {
 
     private Long id;
-    private String name;
+    private String tag;
+    private Long usedCount;
+    private Date dateCreated;
+    private Date lastUpdated;
 
+    @Override
+    public String toString() {
+        return "Tag{" +
+                "id=" + id +
+                ", tag='" + tag + '\'' +
+                '}';
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUsedCount() {
+        return usedCount;
+    }
+
+    public void setUsedCount(Long usedCount) {
+        this.usedCount = usedCount;
+    }
 }
