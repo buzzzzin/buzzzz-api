63af337fa4282b878b11dc9ac1f2de784df36973
Jeevesh Pandey <jeevesh.pandey@tothenew.com>
2015-09-25 23:41:22 +0530

    Modification:Added userId and userName in Buzz.

--- src/main/java/in/buzzzz/domain/buzz/Buzz.java
+++ src/main/java/in/buzzzz/domain/buzz/Buzz.java
@@ -19,6 +19,8 @@ public class Buzz {
     private Schedule schedule;
     private List<String> tags;
     private List<String> interests;
+    private String userId;
+    private String email;
 
     @Override
     public String toString() {
