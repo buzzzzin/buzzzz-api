18367d3e22ac3813892f86aab4479269e8adbbf8
Ekansh <ekansh.rastogi@intelligrape.com>
2015-09-26 01:35:29 +0530

    auth response update

--- src/main/java/in/buzzzz/v1/service/auth/AuthenticationService.java
+++ src/main/java/in/buzzzz/v1/service/auth/AuthenticationService.java
@@ -23,32 +23,22 @@ public class AuthenticationService {
     public LoginDto login(UserCommand userCommand){
         User user = userRepository.findByEmail(userCommand.getEmail());
         LoginDto loginDto = new LoginDto();
-        System.out.println("11111111111111111");
         if(user==null){
             user = userRepository.save(new User(userCommand));
             loginDto.setUser(user.convertToDto());
             loginDto.setHasInterests(false);
-            System.out.println("aaaaaaaaaaaaaaaaaa");
         }
         else{
             loginDto.setUser(user.convertToDto());
             loginDto.setHasInterests(user.getInterests().size()!=0);
-            System.out.println("bbbbbbbbbbbbbbbbbbbbbb");
         }
         UserAuthMapping userAuthMapping = userAuthMappingRepository.findByEmail(user.getEmail());
-        System.out.println("22222222222222222222222222");
         if(userAuthMapping==null){
-            System.out.println("aaaaaaaaaaaaaaaaaaaaa");
             userAuthMapping = new UserAuthMapping();
-            System.out.println("xxxx");
             userAuthMapping.setAuthToken((new Date()).hashCode() + "" + user.getEmail().hashCode());
-            System.out.println("yyy");
             userAuthMapping.setEmail(user.getEmail());
-            System.out.println("zzz" + (userAuthMapping ==null) + "" + (userAuthMappingRepository==null));
             userAuthMappingRepository.save(userAuthMapping);
-            System.out.println("vbbbbbbbbbbbbbbbbbbbbbbbbb");
         }
-        System.out.println("33333333333333333333");
         loginDto.setAuthToken("token");
         return loginDto;
     }
