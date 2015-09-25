b4288c925c43a14ea874d86ed5dfa6a8c51598f5
Jeevesh Pandey <jeevesh.pandey@tothenew.com>
2015-09-25 23:02:23 +0530

    Modification:Added fields in buzz command.

--- src/main/java/in/buzzzz/v1/co/buzz/BuzzCommand.java
+++ src/main/java/in/buzzzz/v1/co/buzz/BuzzCommand.java
@@ -1,8 +1,18 @@
 package in.buzzzz.v1.co.buzz;
 
 import in.buzzzz.co.ValidateCommand;
+import in.buzzzz.domain.buzz.Schedule;
 
 public class BuzzCommand implements ValidateCommand {
+
+    private String name;
+    private String imageName;
+    private Boolean isRSVP;
+    private String latitude;
+    private String longitude;
+    private String startTime;
+    private String endTime;
+    private Schedule.Period period;
 
     @Override
     public boolean validate() {
