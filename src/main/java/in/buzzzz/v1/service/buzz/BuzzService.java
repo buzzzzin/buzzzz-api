2eea650448b69fba0a9997d7d50d2c3f04f01ed8
sanjeev <sanjeev.jha@intelligrape.com>
2015-09-26 02:45:56 +0530

    added function call to save tags

--- src/main/java/in/buzzzz/v1/service/buzz/BuzzService.java
+++ src/main/java/in/buzzzz/v1/service/buzz/BuzzService.java
@@ -6,6 +6,7 @@ import in.buzzzz.util.exceptions.GenericException;
 import in.buzzzz.util.exceptions.buzz.BuzzNotCreateException;
 import in.buzzzz.v1.co.buzz.BuzzCommand;
 import in.buzzzz.v1.data.buzz.BuzzDto;
+import in.buzzzz.v1.service.tag.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,11 +17,14 @@ public class BuzzService {
 
     @Autowired
     private BuzzRepository buzzRepository;
+    private TagService tagService;
 
     public BuzzDto save(BuzzCommand buzzCommand) throws GenericException, ParseException {
         if (buzzCommand.validate()) {
             Buzz buzz = new Buzz(buzzCommand);
             buzzRepository.save(buzz);
+            //TODO need to apply rabbit MQ call here
+            tagService.createOrUpdateTags(buzzCommand.getTags());
             return buzz.convertToDto();
         }
         throw new BuzzNotCreateException();
