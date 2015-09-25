1c9a9049d72f20bd5bbbbf9cda6b51eef74fd1e4
sanjeev <sanjeev.jha@intelligrape.com>
2015-09-25 23:13:07 +0530

    created domain BuzzTagMapping class

---
+++ src/main/java/in/buzzzz/domain/tag/BuzzTagMapping.java
@@ -0,0 +1,42 @@
+package in.buzzzz.domain.tag;
+
+public class BuzzTagMapping {
+
+    private Long id;
+    private Long buzzId;
+    private String buzzName;
+    private String tag;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getBuzzId() {
+        return buzzId;
+    }
+
+    public void setBuzzId(Long buzzId) {
+        this.buzzId = buzzId;
+    }
+
+    public String getBuzzName() {
+        return buzzName;
+    }
+
+    public void setBuzzName(String buzzName) {
+        this.buzzName = buzzName;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+
+}
