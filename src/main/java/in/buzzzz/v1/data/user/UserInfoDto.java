d85ce0db400ebd1303bdc86b077f4eedf3d2bda3
Ekansh <ekansh.rastogi@intelligrape.com>
2015-09-25 22:37:12 +0530

    user domian updated

--- src/main/java/in/buzzzz/v1/data/user/UserInfoDto.java
+++ src/main/java/in/buzzzz/v1/data/user/UserInfoDto.java
@@ -1,6 +1,7 @@
 package in.buzzzz.v1.data.user;
 
 import in.buzzzz.data.user.UserData;
+import in.buzzzz.domain.user.RegistrationMedium;
 import in.buzzzz.domain.user.User;
 
 /**
@@ -10,10 +11,65 @@ public class UserInfoDto implements UserData {
 
     private String id;
     private String name;
-    private User.Gendre gendre;
+    private User.Gender gender;
     private String email;
     private String mobile;
     private String country;
+    private RegistrationMedium medium;
 
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User.Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(User.Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public RegistrationMedium getMedium() {
+        return medium;
+    }
+
+    public void setMedium(RegistrationMedium medium) {
+        this.medium = medium;
+    }
 }
