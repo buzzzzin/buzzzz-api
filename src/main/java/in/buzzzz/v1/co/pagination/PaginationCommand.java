47e1d4b3404a46243af442323d80735cfb694b2f
Jeevesh Pandey <jeevesh.pandey@tothenew.com>
2015-09-26 16:25:59 +0530

    Modification:Added pagination command and refactor code.

---
+++ src/main/java/in/buzzzz/v1/co/pagination/PaginationCommand.java
@@ -0,0 +1,14 @@
+package in.buzzzz.v1.co.pagination;
+
+public class PaginationCommand {
+
+    private String nextCursor;
+
+    public String getNextCursor() {
+        return nextCursor;
+    }
+
+    public void setNextCursor(String nextCursor) {
+        this.nextCursor = nextCursor;
+    }
+}
