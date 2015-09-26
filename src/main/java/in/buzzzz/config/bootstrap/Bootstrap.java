28a423bee5e06af4649893baf3127f4bbae8cf1d
Jeevesh Pandey <jeevesh.pandey@tothenew.com>
2015-09-26 19:03:54 +0530

    Modification:Added interest bootstrap.

--- src/main/java/in/buzzzz/config/bootstrap/Bootstrap.java
+++ src/main/java/in/buzzzz/config/bootstrap/Bootstrap.java
@@ -20,31 +20,62 @@ public class Bootstrap implements InitializingBean {
         System.out.println("   START BOOTSTRAP");
         System.out.println("###########################");
         try {
-            if (interestRepository.findByName("One") == null) {
+            if (interestRepository.findByName("Blood") == null) {
                 Interest interest = new Interest();
-                interest.setName("One");
-                interest.setImage("One");
+                interest.setName("Blood");
+                interest.setImage("v1443273588/interest/blood.png");
                 interest.setDateCreated(new Date());
                 interest.setTrending(true);
+                interest.setUsedCount(0l);
                 interestRepository.save(interest);
-                for (int i = 0; i <= 10; i++) {
-                    Interest interest1 = new Interest();
-                    interest1.setName("Interest" + i);
-                    interest1.setImage("Interest" + i);
-                    interest1.setDateCreated(new Date());
-                    interest1.setTrending(true);
-                    interestRepository.save(interest1);
-                }
-            }
-            if (interestRepository.count() == 0) {
-                for (int i = 0; i <= 20; i++) {
-                    Interest interest = new Interest();
-                    interest.setName("Interest" + i);
-                    interest.setImage("Interest" + i);
-                    interest.setUsedCount(0L);
-                    interest.setDateCreated(new Date());
-                    interestRepository.save(interest);
-                }
+
+                Interest interest2 = new Interest();
+                interest2.setName("Music");
+                interest2.setImage("v1443273587/interest/music.png");
+                interest2.setDateCreated(new Date());
+                interest2.setTrending(true);
+                interest2.setUsedCount(0l);
+                interestRepository.save(interest2);
+
+                Interest interest7 = new Interest();
+                interest7.setName("Coding");
+                interest7.setImage("v1443274393/interest/coading.jpg");
+                interest7.setDateCreated(new Date());
+                interest7.setTrending(true);
+                interest7.setUsedCount(0l);
+                interestRepository.save(interest7);
+
+                Interest interest6 = new Interest();
+                interest6.setName("Party");
+                interest6.setImage("v1443274270/interest/party.jpg");
+                interest6.setDateCreated(new Date());
+                interest6.setTrending(true);
+                interest6.setUsedCount(0l);
+                interestRepository.save(interest6);
+
+                Interest interest5 = new Interest();
+                interest5.setName("Running");
+                interest5.setImage("v1443274165/interest/running.jpg");
+                interest5.setDateCreated(new Date());
+                interest5.setTrending(true);
+                interest5.setUsedCount(0l);
+                interestRepository.save(interest5);
+
+                Interest interest3 = new Interest();
+                interest3.setName("Sports");
+                interest3.setImage("v1443273587/interest/sports.jpg");
+                interest3.setDateCreated(new Date());
+                interest3.setTrending(true);
+                interest3.setUsedCount(0l);
+                interestRepository.save(interest3);
+
+                Interest interest4 = new Interest();
+                interest4.setName("Technology");
+                interest4.setImage("v1443273587/interest/technology.jpg");
+                interest4.setDateCreated(new Date());
+                interest4.setTrending(true);
+                interest4.setUsedCount(0l);
+                interestRepository.save(interest4);
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
