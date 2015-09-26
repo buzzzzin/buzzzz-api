9a9c25d3aa0dec7889702104a6dcb46672d80428
sanjeev <sanjeev.jha@intelligrape.com>
2015-09-26 11:36:39 +0530

    refactored

--- src/main/java/in/buzzzz/repository/interest/InterestRepository.java
+++ src/main/java/in/buzzzz/repository/interest/InterestRepository.java
@@ -1,12 +1,11 @@
 package in.buzzzz.repository.interest;
 
-import in.buzzzz.domain.buzz.Buzz;
 import in.buzzzz.domain.interest.Interest;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface InterestRepository extends MongoRepository<Interest, String> {
 
-    Buzz findById(String id);
+    Interest findById(String id);
 
-    Buzz findByName(String name);
+    Interest findByName(String name);
 }
