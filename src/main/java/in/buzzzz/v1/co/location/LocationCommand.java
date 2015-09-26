a1a1350f30f9e2480f47d0b7ac9ebde65c2afa44
Ekansh <ekansh.rastogi@intelligrape.com>
2015-09-26 08:57:26 +0530

    change location command lat and long to double

--- src/main/java/in/buzzzz/v1/co/location/LocationCommand.java
+++ src/main/java/in/buzzzz/v1/co/location/LocationCommand.java
@@ -5,8 +5,8 @@ package in.buzzzz.v1.co.location;
  */
 public class LocationCommand {
 
-    private String latitude;
-    private String longitude;
+    private Double latitude;
+    private Double longitude;
     private Integer radius;
 
     @Override
@@ -18,19 +18,19 @@ public class LocationCommand {
                 '}';
     }
 
-    public String getLatitude() {
+    public Double getLatitude() {
         return latitude;
     }
 
-    public void setLatitude(String latitude) {
+    public void setLatitude(Double latitude) {
         this.latitude = latitude;
     }
 
-    public String getLongitude() {
+    public Double getLongitude() {
         return longitude;
     }
 
-    public void setLongitude(String longitude) {
+    public void setLongitude(Double longitude) {
         this.longitude = longitude;
     }
 
